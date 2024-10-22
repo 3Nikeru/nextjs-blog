b27a2021973bbb96c718ac8998f2c5d5683b1e9f
Alina Nikeru <alina.nikeru@gmail.com>
2024-10-22 19:58:12 +0300

    commit

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import styles from './singlePost.module.css'
 import PostUser from "@/components/PostUser/page"
+import PostImageClient from "@/components/PostImageClient";
 
 const getTotalPosts = async(slug) => {
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
@@ -8,15 +9,34 @@ const getTotalPosts = async(slug) => {
   return response.json();
 }
 
-const singlePage = async({params}) => {
+const singlePage = async({params, searchParams}) => {
   const {slug} = params;
   const post = await getTotalPosts(slug);
-  // console.log(params.slug);
+  const imageUrl = searchParams.imageUrl || '/post.jpg';
   
   return (
-    <div>
-      <Image/>
-      <PostUser userId={post.id}/>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <PostImageClient imageUrl={imageUrl} failBackImage='/post.jpg' alt={post.title} className={styles.img}/>
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>{post.title}</h1>
+        <div className={styles.detail}>
+          <Image 
+            src='/avatar.jpg'
+            alt="avatar"
+            className={styles.avatar}
+            width={64}
+            height={64}
+            />
+            <PostUser userId={post.id}/>
+        </div>
+        <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailTitle}>01.01.2024</span>
+        </div>
+        <div className={styles.content}>{post.body}</div>
+      </div>
     </div>
   )
 }
--- components/PostCard/page.jsx
+++ components/PostCard/page.jsx
@@ -6,8 +6,8 @@ const PostCard = async({post, index}) => {
   const accessKey = process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY
   const baseUrl = "https://api.unsplash.com/search/photos"
   try {
-    const query = encodeURIComponent(post.title) || 'nature';
-    const images = await fetch(`${baseUrl}?query=${query}&client_id=${accessKey}&per_page=10`)
+    const query = encodeURIComponent(post.title) || 'cars';
+    const images = await fetch(`${baseUrl}?query=${query}&per_page=30&client_id=${accessKey}&per_page=10`)
     if(!images.ok){
       const errorText = await images.text();
       console.log(`${errorText}`);
@@ -20,13 +20,13 @@ const PostCard = async({post, index}) => {
       <div className={styles.container}>
         <div className={styles.top}>
           <div className={styles.imgContainer}>
-              {/* <Image
+              <Image
                   fill
                   src={imageUrl}
                   alt="post"
                   sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
                   className={styles.img}
-              /> */}
+              />
               {/* <img src={imageUrl} alt={post.title} className={styles.img}/> */}
           </div>
           <span className={styles.data}>03/10/2024</span>
@@ -34,7 +34,10 @@ const PostCard = async({post, index}) => {
         <div className={styles.button}>
           <h1 className={styles.title}>{post.title.slice(0, 25)}...</h1>
           <p className={styles.desc}>{post.body.slice(0, 100)}...</p>
-          <Link href={`/blog/${post.id}`} className={styles.link}>Read me</Link>
+          <Link href={{ 
+            pathname:`/blog/${post.id}`,
+            query: {imageUrl}
+            }} className={styles.link}>Read me</Link>
           </div>
       </div>
     )
