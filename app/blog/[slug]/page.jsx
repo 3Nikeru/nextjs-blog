1750f5ef9cc428f2f9c1684160a5d68d3fa7d5d4
Alina Nikeru <alina.nikeru@gmail.com>
2025-02-06 15:31:03 +0200

    commit

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,23 +1,37 @@
-import Image from "next/image"
-import styles from './singlePost.module.css'
-import PostUser from "@/components/PostUser/page"
+import Image from "next/image";
+import styles from './singlePost.module.css';
+import PostUser from "@/components/PostUser/page";
 import PostImageClient from "@/components/PostImageClient";
 
-const getTotalPosts = async(slug) => {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
-  if(!response.ok) throw new Error('something went wrong');
-  return response.json();
-}
+const getAllSlugs = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!response.ok) throw new Error('Failed to fetch posts');
+  const posts = await response.json();
+  return posts.map(post => post.id.toString());
+};
 
-const singlePage = async({params, searchParams}) => {
-  const {slug} = params;
+export const generateStaticParams = async () => {
+  const slugs = await getAllSlugs();
+  return slugs.map(slug => ({
+    slug: slug,
+  }));
+};
+
+const getTotalPosts = async (slug) => {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`);
+  if (!response.ok) throw new Error('Something went wrong');
+  return response.json();
+};
+
+const singlePage = async ({ params, searchParams }) => {
+  const { slug } = params;
   const post = await getTotalPosts(slug);
   const imageUrl = searchParams.imageUrl || '/post.jpg';
-  
+
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
-        <PostImageClient imageUrl={imageUrl} failBackImage='/post.jpg' alt={post.title} className={styles.img}/>
+        <PostImageClient imageUrl={imageUrl} failBackImage='/post.jpg' alt={post.title} className={styles.img} />
       </div>
       <div className={styles.textContainer}>
         <h1 className={styles.title}>{post.title}</h1>
@@ -28,17 +42,17 @@ const singlePage = async({params, searchParams}) => {
             className={styles.avatar}
             width={64}
             height={64}
-            />
-            <PostUser userId={post.userId}/>
+          />
+          <PostUser userId={post.userId} />
         </div>
         <div className={styles.detailText}>
-            <span className={styles.detailTitle}>Published</span>
-            <span className={styles.detailTitle}>01.01.2024</span>
+          <span className={styles.detailTitle}>Published</span>
+          <span className={styles.detailTitle}>01.01.2024</span>
         </div>
         <div className={styles.content}>{post.body}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default singlePage
+export default singlePage;
